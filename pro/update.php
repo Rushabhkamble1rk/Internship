c87becdc643070d450be9b45e6a2d0e9aea19635
Rushabhkamble1rk <81916956+Rushabhkamble1rk@users.noreply.github.com>
2021-04-05 11:46:46 +0530

    Add files via upload

---
+++ pro/update.php
@@ -0,0 +1,28 @@
+ <?php
+ 		 include 'database.php';
+		$id=$_POST['id'];
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$city=$_POST['city'];
+		
+		$sql = "UPDATE `crud` SET `name`='$name',`email`='$email',`phone`='$phone',`city`='$city' WHERE id=$id";
+		if (mysqli_query($conn, $sql)) {
+			echo json_encode(array("statusCode"=>200));
+		} 
+		else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+		mysqli_close($conn);
+
+
+		$id=$_POST['id'];
+		$sql = "DELETE FROM `crud` WHERE id=$id ";
+		if (mysqli_query($conn, $sql)) {
+			echo $id;
+		} 
+		else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+
+	?>
